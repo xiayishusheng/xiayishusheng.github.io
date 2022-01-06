e328e6d2274ee68fd5028b0282468851658a4ec9
侠义书生 <564106097@qq.com>
2022-01-06 23:27:53 +0800

    Site updated: 2022-01-06 23:27:48

--- js/xiayishusheng.js
+++ js/xiayishusheng.js
@@ -1,1 +1,3 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>function my_log() {
+  console.log("test");
+}
